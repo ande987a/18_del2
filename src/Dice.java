e75c4c4b2e138768003a641fab39fa4ded9910a5
nikolajwassmann <nwassmann@hotmail.com>
2018-11-06 17:47:14 +0100

    Lavet og testet terning klasse

--- src/Dice.java
+++ src/Dice.java
@@ -1,2 +1,8 @@
 public class Dice {
+    public int roll() {
+        float t1 = (float) Math.random();
+        float t2 = t1 * 5;
+        int t3 = Math.round(t2);
+        return t3 + 1;
+    }
 }
