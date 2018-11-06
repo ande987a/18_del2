090762f29996234e9a4bd9adb1301ceee04c8a60
s185114 <s185114@student.dtu.dk>
2018-11-06 22:20:13 +0100

    Har lavet en Dice_cup metode... har dog placeret den under klassen dice, eftersom jeg ikke mener den burde få sin egen klasse, da den jo også omhandler terningerne.

--- src/Dice.java
+++ src/Dice.java
@@ -1,8 +1,13 @@
 public class Dice {
-    public int roll() {
+    public static int roll() {
         float t1 = (float) Math.random();
         float t2 = t1 * 5;
         int t3 = Math.round(t2);
         return t3 + 1;
     }
+
+    public static int cup() {
+        return Dice.roll() + Dice.roll();
+    }
+
 }
