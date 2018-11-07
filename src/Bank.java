a0b435e5c90c3952edef7d4c432364ecc53e7982
Anders <s185100@student.dtu.dk>
2018-11-07 16:12:46 +0100

    Banken er nu addet og spillerene vinder/taber penge ved at lande på de enkelte felter

--- src/Bank.java
+++ src/Bank.java
@@ -1,2 +1,20 @@
 public class Bank {
+
+    private int balance1;
+    public int getBalance1(){
+        return this.balance1;
+    }
+    public void setBalance1(int balance1){
+        this.balance1 = balance1;
+    }
+
+    private int balance2;
+    public int getBalance2(){
+        return this.balance2;
+    }
+    public void setBalance2(int balance2) {
+        this.balance2 = balance2;
+    }
+
 }
+
--- src/Game.java
+++ src/Game.java
@@ -1,22 +1,71 @@
-import java.util.Scanner;
-
 public class Game {
     public static void main(String[] args) {
 
         Player Player1 = new Player();
         Player Player2 = new Player();
 
+        Bank b = new Bank();
+
+
         while(!Player.Win()){
             System.out.println();
             System.out.println("Spiller nr.1 kaster");
             Player1.getturn();
+            switch (Player1.getfield()-1) {
+                case 1: b.setBalance1(b.getBalance1()+250);
+                    break;
+                case 2: b.setBalance1(b.getBalance1()-100);
+                    break;
+                case 3: b.setBalance1(b.getBalance1()+100);
+                    break;
+                case 4: b.setBalance1(b.getBalance1()-20);
+                    break;
+                case 5: b.setBalance1(b.getBalance1()+180);
+                    break;
+                case 6: b.setBalance1(b.getBalance1()+0);
+                    break;
+                case 7: b.setBalance1(b.getBalance1()-70);
+                    break;
+                case 8: b.setBalance1(b.getBalance1()+60);
+                    break;
+                case 9: b.setBalance1(b.getBalance1()-80);
+                    break;
+                case 10: b.setBalance1(b.getBalance1()-50);
+                    break;
+                case 11: b.setBalance1(b.getBalance1()+650);
+                    break;}
+            System.out.println(b.getBalance1());
             //TODO Her skal muligvis være en get til den opdaterede kapital
 
         if(!Player.Win()){
             System.out.println();
             System.out.println("Spiller nr.2 kaster");
-            //TODO Her skal muligvis være en get til den opdaterede kapital-
             Player2.getturn();
+            //TODO Her skal muligvis være en get til den opdaterede kapital-
+            switch (Player2.getfield()-1) {
+                case 1: b.setBalance2(b.getBalance2()+250);
+                    break;
+                case 2: b.setBalance2(b.getBalance2()-100);
+                    break;
+                case 3: b.setBalance2(b.getBalance2()+100);
+                    break;
+                case 4: b.setBalance2(b.getBalance2()-20);
+                    break;
+                case 5: b.setBalance2(b.getBalance2()+180);
+                    break;
+                case 6: b.setBalance2(b.getBalance2()+0);
+                    break;
+                case 7: b.setBalance2(b.getBalance2()-70);
+                    break;
+                case 8: b.setBalance2(b.getBalance2()+60);
+                    break;
+                case 9: b.setBalance2(b.getBalance2()-80);
+                    break;
+                case 10: b.setBalance2(b.getBalance2()-50);
+                    break;
+                case 11: b.setBalance2(b.getBalance2()+650);
+                    break;}
+            System.out.println(b.getBalance2());
         }
         }
         Game.Won();
